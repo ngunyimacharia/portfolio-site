000e290d4b24c2ffd1cf978b12acbc875b9f22fa
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2020-01-14 18:37:40 +0300

    Fetch sitename and sitedescription on homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,10 +5,10 @@
         <div class="column"></div>
         <div class="column is-three-fifths has-text-centered">
           <g-image src="~/assets/images/ppic.png" width="200" class="is-round-image has-margin-20" />
-          <h1 class="has-text-weight-bold is-capitalized is-size-5">KELVIN M NGUNYI</h1>
+          <h1 class="has-text-weight-bold is-uppercase is-size-5">{{$page.metadata.siteName}}</h1>
           <h3
             class="has-text-weight-semibold is-capitalized is-size-6 has-margin-20"
-          >Full-time Software Engineer and aspiring tech rockstar!</h3>
+          >{{$page.metadata.siteDescription}}</h3>
           <p
             class="has-text-weight-light is-capitalized is-size-6 has-margin-20"
           >Design, development, integration and maintenance of web-based systems using the latest available technologies.</p>
@@ -40,3 +40,12 @@ export default {
   }
 };
 </script>
+
+<page-query>
+    query metadata{
+        metadata{
+            siteName
+            siteDescription
+        }
+    }
+</page-query>
