dc2cd17117d02d167746a312c32dd74ab41f452e
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2020-01-14 18:17:19 +0300

    Markdown plugins

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,21 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: 'Kelvin M Ngunyi',
-    plugins: []
-}
+    siteName: "Kelvin M Ngunyi",
+    siteDescription: "Full-time Software Engineer and aspiring tech rockstar!",
+    plugins: [{
+        use: "@gridsome/source-filesystem",
+        options: {
+            typeName: "Platform",
+            baseDir: "./content/platforms",
+            path: "*.md"
+        }
+    }],
+    templates: {
+        Platform: "/platform/:title"
+    }
+};
