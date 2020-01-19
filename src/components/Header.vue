34f519f33c65259e7bd4eab4dce999a9b0a3d63d
Kelvin Macharia <ngunyimacharia@gmail.com>
2020-01-19 18:35:13 +0300

    Header, logo and favicon improvements

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,8 @@
   <header class="header">
     <b-navbar class="navbar" :transparent="true" fixed-top>
       <template slot="brand">
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <span class="logo">K | N</span>
+        <b-navbar-item tag="router-link" class="is-paddingless" :to="{ path: '/' }">
+          <g-image src="~/assets/images/logo.png" width="200" height="50" alt="Logo" class="logo" />
         </b-navbar-item>
       </template>
 
@@ -14,15 +14,24 @@
             Portfolio
           </g-link>
         </b-navbar-item>
+        <b-navbar-item class="empty-nav-item">
+          <!-- Nothing -->
+        </b-navbar-item>
         <b-navbar-item>
           <g-link to="/experience/" class="link is-size-7">
             <span class="has-text-white">02.</span>&nbsp;Experience
           </g-link>
         </b-navbar-item>
+        <b-navbar-item class="empty-nav-item">
+          <!-- Nothing -->
+        </b-navbar-item>
         <b-navbar-item>
           <g-link to="/education/" class="link is-size-7">
             <span class="has-text-white">03.</span>&nbsp;Education
           </g-link>
+        </b-navbar-item>
+        <b-navbar-item class="empty-nav-item">
+          <!-- Nothing -->
         </b-navbar-item>
         <b-navbar-item>
           <g-link to="/contact/" class="link is-size-7">
@@ -33,3 +42,12 @@
     </b-navbar>
   </header>
 </template>
+
+<style scoped>
+.logo {
+  max-height: 50px;
+}
+.empty-nav-item{
+  display: none;
+}
+</style>
