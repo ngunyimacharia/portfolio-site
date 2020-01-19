79a6df494e981db69a0ff4d449e3b2b61cef7dcd
Kelvin Macharia <ngunyimacharia@gmail.com>
2020-01-19 18:34:59 +0300

    Contact form edit and validation

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -5,29 +5,24 @@
     </Hero>
 
     <section class="container has-margin-top-20">
-      <form action method="POST">
+      <form action method="POST" v-on:submit.prevent="submit">
         <b-field label="Name" labelPosition="inside">
-          <b-input required v-model="name" placeholder="What shall we call you?"></b-input>
+          <b-input required v-model="message.name" placeholder="What shall we call you?"></b-input>
         </b-field>
 
         <b-field label="Email" labelPosition="inside">
-          <b-input required v-model="email" placeholder="Everyone has one of these nowadays"></b-input>
-        </b-field>
-
-        <b-field label="Country" labelPosition="inside" expanded>
-          <b-select required placeholder="Select country" expanded size="is-small" v-model="country">
-            <option
-              v-for="country in $page.countries"
-              :key="country.code"
-              :value="country.code"
-            >{{country.name}}</option>
-          </b-select>
+          <b-input
+            required
+            type="email"
+            v-model="message.email"
+            placeholder="Everyone has one of these nowadays"
+          ></b-input>
         </b-field>
 
         <b-field label="Subject" labelPosition="inside">
           <b-input
             required
-            v-model="subject"
+            v-model="message.subject"
             placeholder="If you had only 5 words, what would you say?"
           ></b-input>
         </b-field>
@@ -36,13 +31,19 @@
           <b-input
             type="textarea"
             required
-            v-model="message"
+            v-model="message.message"
             maxlength="100"
             placeholder="Tell me more..."
           ></b-input>
         </b-field>
         <div>
-          <b-button class="is-success is-rounded is-pulled-right" @click="submit">Send Message</b-button>
+          <input
+            v-if="!loading"
+            class="button is-success is-rounded is-pulled-right"
+            type="submit"
+            value="Send Message"
+          />
+          <button v-else class="button is-success is-rounded is-pulled-right is-loading">Loading</button>
         </div>
       </form>
       <br />
@@ -63,41 +64,44 @@ export default {
   },
   data() {
     return {
-      name: "",
-      email: "",
-      country: null,
-      subject: "",
-      message: ""
+      loading: false,
+      message: {
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+      }
     };
   },
   methods: {
     submit() {
+      this.loading = true;
       this.axios
         .post("https://getform.io/f/23ccbb3c-2f75-4941-a585-4b16df64c0b7", {
-          name: this.name,
-          email: this.email,
-          country: this.country,
-          subject: this.subject,
-          message: this.message
+          name: this.message.name,
+          email: this.message.email,
+          subject: this.message.subject,
+          message: this.message.message
         })
 
         .then(response => {
+          this.loading = false;
           this.$buefy.toast.open({
             message: "I have received your message. Thank you!",
             type: "is-success"
           });
-          this.name = "";
-          this.email = "";
-          this.country = null;
-          this.subject = "";
-          this.message = "";
+          this.message.name = "";
+          this.message.email = "";
+          this.message.subject = "";
+          this.message.message = "";
         })
 
         .catch(error => {
+          this.loading = false;
           this.$buefy.toast.open({
             message:
               "There was a problem sending your message. Please try again.",
-            type: "is-success"
+            type: "is-danger"
           });
           console.log(error);
         });
